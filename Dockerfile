ded9a4624c0257b1de34b6fe9be82e718bac253e
Alex Vanderpot <alex@vanderpot.com>
2021-03-31 16:41:25 -0700

    openbox

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,9 @@
 FROM ubuntu:xenial
-LABEL maintainer "ARASHI Jumpei"
 EXPOSE 5900 6099
 ENV \
   DEBIAN_FRONTEND="nonintractive" \
   X11VNC_PASSWORD="password"
 RUN apt-get update -y
-RUN apt-get install -y xvfb x11vnc fluxbox
+RUN apt-get install --no-install-recommends -y xvfb x11vnc xserver-xorg-video-all xserver-xorg-input-all xserver-xorg-core xinit x11-xserver-utils chromium-browser
 ADD ./entrypoint.sh /opt/entrypoint.sh
 ENTRYPOINT /opt/entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,12 @@
-#! /bin/sh
+#!/usr/bin/env bash
+set -euox pipefail
 
-Xvfb :99 -ac -listen tcp -screen 0 2560x1440x24 &
+Xvfb :99 -ac -listen tcp -screen 0 ${X11VNC_RESOLUTION:-2560x1440}x24 &
 sleep 3
-/usr/bin/fluxbox -display :99 -screen 0 &
+DISPLAY=:99 /usr/bin/openbox-session &
 sleep 3
+DISPLAY=:99 chromium-browser "${KIOSK_URL}" --no-sandbox --start-fullscreen --kiosk --incognito --noerrdialogs \
+    --disable-translate --no-first-run --fast --fast-start \
+    --disable-infobars --disable-features=TranslateUI \
+    --disk-cache-dir=/dev/null --password-store=basic &
 x11vnc -display :99.0 -forever -passwd ${X11VNC_PASSWORD:-password} 
