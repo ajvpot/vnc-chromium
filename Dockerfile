7481737cf8ca83cf885d4cb9896c6c579b708da3
Alex Vanderpot <alex@vanderpot.com>
2021-03-31 17:11:53 -0700

    chrome policies

--- Dockerfile
+++ Dockerfile
@@ -5,5 +5,6 @@ ENV \
   X11VNC_PASSWORD="password"
 RUN apt-get update -y
 RUN apt-get install --no-install-recommends -y xvfb x11vnc xserver-xorg-video-all xserver-xorg-input-all xserver-xorg-core xinit x11-xserver-utils chromium-browser
+ADD ./chrome.json /etc/chromium-browser/policies/managed/chrome.json
 ADD ./entrypoint.sh /opt/entrypoint.sh
 ENTRYPOINT /opt/entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 set -euox pipefail
 
+RESCOMMA=`echo "${X11VNC_RESOLUTION:-2560x1440}" | sed -e "s|x|,|"`
+
 Xvfb :99 -ac -listen tcp -screen 0 ${X11VNC_RESOLUTION:-2560x1440}x24 &
 sleep 3
-DISPLAY=:99 /usr/bin/openbox-session &
-sleep 3
-DISPLAY=:99 chromium-browser "${KIOSK_URL}" --no-sandbox --start-fullscreen --kiosk --incognito --noerrdialogs \
-    --disable-translate --no-first-run --fast --fast-start \
-    --disable-infobars --disable-features=TranslateUI \
-    --disk-cache-dir=/dev/null --password-store=basic &
+DISPLAY=:99 chromium-browser "${KIOSK_URL}" --window-size=$RESCOMMA --start-fullscreen --kiosk --incognito --noerrdialogs --no-sandbox \
+    --window-position=0,0 --disable-translate --no-first-run --fast --fast-start \
+    --disable-infobars --disable-features=TranslateUI --password-store=basic &
 x11vnc -display :99.0 -forever -passwd ${X11VNC_PASSWORD:-password} 
